dd6951fff21f796feab9fde407ac442366674d77
mitesh-bhoite <mitesh07bhoite@gmail.com>
2025-03-14 10:37:40 +0530

    file system module

---
+++ fsmodule.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+fs.readFile("file.txt", "utf8", (err, data) => {
+  //console.log(err, data);
+});
+//2nd
+//note : sync is basic ly use for lock that function and execute that one first than next
+const a = fs.readFileSync("file.txt");
+console.log(a.toString());
+//same for write
+fs.writeFile("file.txt", "Front-end developer", () => {
+  console.log("Written to the file");
+});
