cde5d0aa549dab250723802824d7f4e3006d9688
mitesh-bhoite <mitesh07bhoite@gmail.com>
2025-03-17 22:49:28 +0530

    comments

--- pathmodule.js
+++ pathmodule.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const a = path.basename("C:\\tenp\\myfile.html");
 const a2 = path.dirname("C:\\tenp\\myfile.html");
-//console.log(a);
+console.log(a);
 console.log(a2);
 const a3 = path.extname(__filename);
 console.log(a3);
