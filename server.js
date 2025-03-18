4f651585ea672af3754fefe41cb521f77dee7ae3
mitesh-bhoite <mitesh07bhoite@gmail.com>
2025-03-18 22:26:20 +0530

    code for server

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 //it is for server module
 const http = require("http");
-
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
   res.end("Hello, Node.js Server!");
