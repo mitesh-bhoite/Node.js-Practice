f039cb99bf6e2b952b0dcbcf0a20c6dbf1537bb3
mitesh-bhoite <mitesh07bhoite@gmail.com>
2025-03-19 23:05:58 +0530

    os practice

--- osmodules.js
+++ osmodules.js
@@ -1,3 +1,3 @@
-//in the os module we can check all information about os of our device
+//in the os module we can check all information about os of our device and info of os
 const os = require("os");
 console.log(os.freemem());
