44e9009751ada8743a7b510291d2fb366333dac8
mitesh-bhoite <mitesh07bhoite@gmail.com>
2025-03-25 22:37:13 +0530

    for  reading file

--- filehandling.js
+++ filehandling.js
@@ -3,3 +3,7 @@ const fs = require("fs");
 
 // Writing to a file
 fs.writeFileSync("message.txt", "Hello, this is a test file.");
+
+// Reading from a file
+const data = fs.readFileSync("message.txt", "utf8");
+console.log(data);
