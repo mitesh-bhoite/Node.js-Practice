a831e3a6dfed9902dfbdce303ee423516db6d357
mitesh-bhoite <mitesh07bhoite@gmail.com>
2025-03-26 21:51:37 +0530

    for basic expree server

--- basic-express-server.js
+++ basic-express-server.js
@@ -1,6 +1,6 @@
 //it is a basic server for express
 const express = require("express");
-const ap = express();
+const app = express();
 
 app.get("/", (req, res) => {
   res.send("Welcome to Express!");
