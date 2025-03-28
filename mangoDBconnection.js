296b5e242247d730204c12566ae09ba66b1024e1
mitesh-bhoite <mitesh07bhoite@gmail.com>
2025-03-28 22:27:25 +0530

    mangoDB connection

--- mangoDBconnection.js
+++ mangoDBconnection.js
@@ -1,1 +1,8 @@
 const mongoose = require("mongoose");
+mongoose
+  .connect("mongodb://localhost:27017/testDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.error(err));
