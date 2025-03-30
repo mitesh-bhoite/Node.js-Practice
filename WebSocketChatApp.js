fbd975963bcfd5f973c88a73d383960145b5fc81
mitesh-bhoite <mitesh07bhoite@gmail.com>
2025-03-30 22:14:25 +0530

    WebSocket Chat App

--- WebSocketChatApp.js
+++ WebSocketChatApp.js
@@ -4,3 +4,13 @@ const { Server } = require("socket.io");
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+  socket.on("message", (msg) => {
+    io.emit("message", msg);
+  });
+  socket.on("disconnect", () => console.log("User disconnected"));
+});
+
+server.listen(3000, () => console.log("Chat server running on port 3000"));
