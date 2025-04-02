02c7928b3e772feaa7b77fd95bef9a81198a057e
mitesh-bhoite <mitesh07bhoite@gmail.com>
2025-04-02 22:39:13 +0530

    Fetch Data from an API

--- Axios.js
+++ Axios.js
@@ -1,4 +1,7 @@
 //Fetch Data from an API
 const axios = require("axios");
 
-axios.get("https://jsonplaceholder.typicode.com/posts");
+axios
+  .get("https://jsonplaceholder.typicode.com/posts")
+  .then((response) => console.log(response.data))
+  .catch((error) => console.error(error));
