3896cb26e72910e255d263a95094a07887972d1b
mitesh-bhoite <mitesh07bhoite@gmail.com>
2025-04-03 23:02:34 +0530

    URL Shortener

--- URLShortener.js
+++ URLShortener.js
@@ -3,3 +3,12 @@ const shortid = require("shortid");
 
 const app = express();
 app.use(express.json());
+
+const urls = {};
+
+app.post("/shorten", (req, res) => {
+  const { longUrl } = req.body;
+  const shortUrl = shortid.generate();
+  urls[shortUrl] = longUrl;
+  res.json({ shortUrl: `http://localhost:3000/${shortUrl}` });
+});
