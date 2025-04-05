2864d36d1ecf9d268b4d5e03dfc4343f646a5ffd
mitesh-bhoite <mitesh07bhoite@gmail.com>
2025-04-05 23:13:25 +0530

    Web Scraper

--- WebScraper.js
+++ WebScraper.js
@@ -3,3 +3,11 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 
 const url = "https://example.com";
+
+axios
+  .get(url)
+  .then((response) => {
+    const $ = cheerio.load(response.data);
+    console.log($("h1").text());
+  })
+  .catch((err) => console.error(err));
