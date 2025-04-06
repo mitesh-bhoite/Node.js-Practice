1081d43d34e6da725482a047f5dfc8f9e676a951
mitesh-bhoite <mitesh07bhoite@gmail.com>
2025-04-06 23:50:20 +0530

    Email Sender

--- EmailSender.js
+++ EmailSender.js
@@ -6,3 +6,14 @@ const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: { user: "your_email@gmail.com", pass: "your_password" },
 });
+const mailOptions = {
+  from: "your_email@gmail.com",
+  to: "receiver_email@gmail.com",
+  subject: "Test Email",
+  text: "Hello from Node.js!",
+};
+
+transporter.sendMail(mailOptions, (err, info) => {
+  if (err) console.error(err);
+  else console.log("Email sent: " + info.response);
+});
