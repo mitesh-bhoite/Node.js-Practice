8a976eb7c5dfb9a67da7d740420e59a77b05f527
mitesh-bhoite <mitesh07bhoite@gmail.com>
2025-04-06 23:48:51 +0530

    Email Sender

--- EmailSender.js
+++ EmailSender.js
@@ -1,3 +1,8 @@
 //Email Sender
 
 const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: { user: "your_email@gmail.com", pass: "your_password" },
+});
