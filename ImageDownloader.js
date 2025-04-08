0ce98b636a7413286dc57919e7546d6ed73afc9b
mitesh-bhoite <mitesh07bhoite@gmail.com>
2025-04-08 23:30:33 +0530

    Image Downloader

--- ImageDownloader.js
+++ ImageDownloader.js
@@ -1,3 +1,8 @@
 //Image Downloader
 const axios = require("axios");
 const fs = require("fs");
+
+const url = "https://example.com/image.jpg";
+axios({ url, responseType: "stream" }).then((response) => {
+  response.data.pipe(fs.createWriteStream("downloaded.jpg"));
+});
