7441df184febd248a62d87942f060d61842ab43b
mitesh-bhoite <mitesh07bhoite@gmail.com>
2025-04-09 21:08:24 +0530

    PDF Generator

--- PDFGenerator.js
+++ PDFGenerator.js
@@ -3,3 +3,6 @@ const pdf = require("pdfkit");
 const fs = require("fs");
 
 const doc = new pdf();
+doc.pipe(fs.createWriteStream("output.pdf"));
+doc.text("Hello, this is a test PDF!", 100, 100);
+doc.end();
