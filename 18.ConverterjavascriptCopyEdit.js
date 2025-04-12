7513bf2436081048d53c258be681051b3450d325
mitesh-bhoite <mitesh07bhoite@gmail.com>
2025-04-12 22:36:18 +0530

    Markdown to HTML Converter

--- 18.ConverterjavascriptCopyEdit.js
+++ 18.ConverterjavascriptCopyEdit.js
@@ -1,3 +1,7 @@
 //Markdown to HTML Converter
 const fs = require("fs");
 const marked = require("marked");
+fs.readFile("README.md", "utf8", (err, data) => {
+  if (err) console.error(err);
+  else fs.writeFileSync("output.html", marked(data));
+});
