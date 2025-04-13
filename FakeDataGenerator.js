7b25b39c8013423a0c0535c420c1fe4bf1587b0e
mitesh-bhoite <mitesh07bhoite@gmail.com>
2025-04-13 23:04:06 +0530

    Fake Data Generator

--- FakeDataGenerator.js
+++ FakeDataGenerator.js
@@ -1,2 +1,8 @@
 //Fake Data Generator
 const faker = require("faker");
+
+for (let i = 0; i < 5; i++) {
+  console.log(
+    `${faker.name.firstName()} ${faker.name.lastName()} - ${faker.internet.email()}`
+  );
+}
