7efd3aa01ec003ec6ac359f85154fae9e7883e0f
mitesh-bhoite <mitesh07bhoite@gmail.com>
2025-04-14 23:03:05 +0530

    Convert CSV to JSON

--- ConvertCSVtoJSON.js
+++ ConvertCSVtoJSON.js
@@ -1,3 +1,9 @@
 //Convert CSV to JSON ...
 const csvtojson = require("csvtojson");
 const fs = require("fs");
+
+csvtojson()
+  .fromFile("data.csv")
+  .then((json) => {
+    fs.writeFileSync("output.json", JSON.stringify(json, null, 2));
+  });
