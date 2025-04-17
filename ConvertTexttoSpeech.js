655ab4117f0111c6f55690fa19ef1405aa208ce9
mitesh-bhoite <mitesh07bhoite@gmail.com>
2025-04-17 23:26:23 +0530

    Convert Text to Speech

--- ConvertTexttoSpeech.js
+++ ConvertTexttoSpeech.js
@@ -1,4 +1,6 @@
 //Convert Text to Speech and Save as Audio
 const say = require("say");
 
-say.export("Hello, this is a test.", "voice.mp3", (err) => {});
+say.export("Hello, this is a test.", "voice.mp3", (err) => {
+  //
+});
