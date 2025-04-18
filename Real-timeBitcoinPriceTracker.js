5b699260f374a758c5adb97aa6be373217243106
mitesh-bhoite <mitesh07bhoite@gmail.com>
2025-04-18 23:56:36 +0530

    Real-time Bitcoin Price Tracker

--- Real-timeBitcoinPriceTracker.js
+++ Real-timeBitcoinPriceTracker.js
@@ -1,4 +1,9 @@
 //Real-time Bitcoin Price Tracker...
 const axios = require("axios");
 
-setInterval(async () => {}, 5000);
+setInterval(async () => {
+  const response = await axios.get(
+    "https://api.coindesk.com/v1/bpi/currentprice.json"
+  );
+  console.log(`Bitcoin Price: ${response.data.bpi.USD.rate}`);
+}, 5000);
