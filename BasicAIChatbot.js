819fe06683ff4949ce686e2b0da420a02f8031bf
mitesh-bhoite <mitesh07bhoite@gmail.com>
2025-04-21 22:58:05 +0530

    Basic AI Chatbot

--- BasicAIChatbot.js
+++ BasicAIChatbot.js
@@ -3,4 +3,7 @@ const axios = require("axios");
 const API_KEY = "YOUR_OPENAI_API_KEY";
 const prompt = "What is Node.js?";
 
-axios;
+axios
+  .post()
+  .then((response) => console.log(response.data.choices[0].text.trim()))
+  .catch((error) => console.error(error));
