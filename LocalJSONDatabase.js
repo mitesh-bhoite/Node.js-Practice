b9f608c57c3bd5898feae68bbc7642a0c1f53a9c
mitesh-bhoite <mitesh07bhoite@gmail.com>
2025-04-22 23:34:23 +0530

    Local JSON Database

--- LocalJSONDatabase.js
+++ LocalJSONDatabase.js
@@ -1,7 +1,7 @@
 //Local JSON Database
 const fs = require("fs");
 
-const users = [{ id: 1, name: "Mitesh", age: 21 }];
-fs.writeFileSync("users.json", JSON.stringify(users, null, 2));
-const data = JSON.parse(fs.readFileSync("users.json", "utf8"));
-console.log(data);
+const users = [
+  { id: 1, name: "Mitesh", age: 21 },
+  { id: 2, name: "John", age: 25 },
+];
