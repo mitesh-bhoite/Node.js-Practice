c1611eeb00c98086c6fac3ba9a17e17735a517ec
mitesh-bhoite <mitesh07bhoite@gmail.com>
2025-04-24 21:43:15 +0530

    Create a Note App with Command Line Interface

--- CommandLineInterface.js
+++ CommandLineInterface.js
@@ -2,4 +2,18 @@
 const fs = require("fs");
 const yargs = require("yargs");
 
-yargs.command({}).parse();
+yargs
+  .command({
+    builder: {
+      title: { type: "string", demandOption: true },
+      body: { type: "string", demandOption: true },
+    },
+    handler(argv) {
+      const note = { title: argv.title, body: argv.body };
+      const notes = JSON.parse(fs.readFileSync("notes.json") || "[]");
+      notes.push(note);
+      fs.writeFileSync("notes.json", JSON.stringify(notes));
+      console.log("Note added!");
+    },
+  })
+  .parse();
