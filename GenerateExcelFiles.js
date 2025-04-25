000edbcc0188ed11e963f327e01aecdb41286c9b
mitesh-bhoite <mitesh07bhoite@gmail.com>
2025-04-25 21:20:45 +0530

    Generate Excel Files

--- GenerateExcelFiles.js
+++ GenerateExcelFiles.js
@@ -4,3 +4,6 @@ const workbook = new ExcelJS.Workbook();
 const sheet = workbook.addWorksheet("Users");
 sheet.addRow(["Name", "Email"]);
 sheet.addRow(["Mitesh", "mitesh@example.com"]);
+workbook.xlsx
+  .writeFile("users.xlsx")
+  .then(() => console.log("Excel file created!"));
