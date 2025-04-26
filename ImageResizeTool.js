82af9a36e4347ed710212cfbea471623b08eaeaf
mitesh-bhoite <mitesh07bhoite@gmail.com>
2025-04-26 21:52:27 +0530

    Image Resize Tool

--- ImageResizeTool.js
+++ ImageResizeTool.js
@@ -1,2 +1,8 @@
 //Image Resize Tool
 const sharp = require("sharp");
+sharp("input.jpg")
+  .resize(300, 300)
+  .toFile("resized.jpg", (err, info) => {
+    if (err) console.error(err);
+    else console.log("Image resized!");
+  });
