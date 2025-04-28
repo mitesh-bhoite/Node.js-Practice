6906cad1d4ccd0fe0377abd6e38a2e34ddbdfe08
mitesh-bhoite <mitesh07bhoite@gmail.com>
2025-04-28 21:13:21 +0530

    File Uploader with Multer

--- FileUploaderwithMulter.js
+++ FileUploaderwithMulter.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const multer = require("multer");
 const app = express();
+
 const upload = multer({ dest: "uploads/" });
 app.post("/upload", upload.single("file"), (req, res) => {
   res.send("File uploaded!");
