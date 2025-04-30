02f275e38e3334e63a501811e17041d611ad2643
mitesh-bhoite <mitesh07bhoite@gmail.com>
2025-04-30 20:27:30 +0530

    Upload to Cloudinary

--- UploadtoCloudinary.js
+++ UploadtoCloudinary.js
@@ -1,7 +1,8 @@
 //Upload to Cloudinary
 const cloudinary = require("cloudinary").v2;
-cloudinary.config({});
-cloudinary.uploader.upload("image.jpg", (err, result) => {
-  if (err) return console.error(err);
-  console.log(result.secure_url);
+cloudinary.config({
+  cloud_name: "your_cloud",
+  api_key: "your_key",
+  api_secret: "your_secret",
 });
+cloudinary.uploader.upload("image.jpg", (err, result) => {});
