e302ff1e1a1b89d8dbcb55aaa977e005d0f41d1a
mitesh-bhoite <mitesh07bhoite@gmail.com>
2025-05-05 23:32:53 +0530

    Convert HTML to PDF

--- ConvertHTMLtoPDF.js
+++ ConvertHTMLtoPDF.js
@@ -1,4 +1,7 @@
 // Convert HTML to PDF
 const pdf = require("html-pdf");
 const html = "<h1>Hello Mitesh!</h1>";
-pdf.create(html).toFile('hello.pdf', (err, res)
+pdf.create(html).toFile("hello.pdf", (err, res) => {
+  if (err) return console.log(err);
+  console.log("PDF created:", res.filename);
+});
