38442bc0bef0fd61622f172f7bd7860765bf3cd3
mitesh-bhoite <mitesh07bhoite@gmail.com>
2025-05-07 12:05:02 +0530

    Command Line Todo App

--- CommandLineTodoApp.js
+++ CommandLineTodoApp.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 let todos = fs.existsSync("todos.json")
   ? JSON.parse(fs.readFileSync("todos.json"))
   : [];
+
 function addTask(task) {
   todos.push(task);
   fs.writeFileSync("todos.json", JSON.stringify(todos));
