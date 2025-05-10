bb45ace35d83443fabfa5a6d82bdf00a72f14c7a
mitesh-bhoite <mitesh07bhoite@gmail.com>
2025-05-10 22:28:21 +0530

    Build a Local Chat CLI App (Net Module)

--- NetModule.js
+++ NetModule.js
@@ -1,5 +1,6 @@
 // Build a Local Chat CLI App (Net Module)
 const net = require("net");
+
 const server = net.createServer((socket) => {
   socket.write("Welcome to CLI Chat!\n");
   socket.on("data", (data) => {
