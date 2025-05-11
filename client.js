c62436fe5b07dadd7a13907370920c57c85a2020
mitesh-bhoite <mitesh07bhoite@gmail.com>
2025-05-11 22:12:57 +0530

    for client.js

--- client.js
+++ client.js
@@ -1,5 +1,6 @@
 //for client.js
 const net = require("net");
+
 const client = net.createConnection({ port: 5000 }, () => {
   console.log("Connected to server");
   process.stdin.on("data", (data) => {
