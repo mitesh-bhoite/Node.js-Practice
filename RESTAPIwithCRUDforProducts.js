e0303bb2bb56a71d87fcf03d903a5fd66693d93e
mitesh-bhoite <mitesh07bhoite@gmail.com>
2025-05-14 21:50:10 +0530

    REST API with CRUD for Products

--- RESTAPIwithCRUDforProducts.js
+++ RESTAPIwithCRUDforProducts.js
@@ -4,6 +4,7 @@ const express = require("express");
 
 const app = express();
 app.use(express.json());
+
 let products = [];
 app.get("/products", (req, res) => res.json(products));
 app.post("/products", (req, res) => {
