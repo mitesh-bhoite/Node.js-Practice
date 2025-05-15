1957ea18b57fa41e47ed6f9b92c037341835dd6b
mitesh-bhoite <mitesh07bhoite@gmail.com>
2025-05-15 22:00:38 +0530

    Mini URL Status Checker

--- MiniURLStatusChecker.js
+++ MiniURLStatusChecker.js
@@ -3,6 +3,7 @@ const axios = require("axios");
 const urls = ["https://google.com", "https://github.com"];
 urls.forEach(async (url) => {
     try{
-        
+        const res = await axios.get(url);
+        console.log(`${url} - ${res.status}`);
     }
 });
