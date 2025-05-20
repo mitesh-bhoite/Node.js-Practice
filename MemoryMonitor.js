09c680f5361e5fb26aa58d89578256a6a17a94de
mitesh-bhoite <mitesh07bhoite@gmail.com>
2025-05-20 22:16:06 +0530

    CPU Temperature and Memory Monitor

--- MemoryMonitor.js
+++ MemoryMonitor.js
@@ -1,2 +1,6 @@
 //CPU Temperature & Memory Monitor (Linux only)
 const { exec } = require("child_process");
+exec("sensors", (err, stdout) => {
+  if (err) return console.log("Run `sudo apt install lm-sensors` first.");
+  console.log(stdout);
+});
