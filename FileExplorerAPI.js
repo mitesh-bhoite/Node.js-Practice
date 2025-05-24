03adc97dcc3c70bfbfc1370d7a649a53c9457887
mitesh-bhoite <mitesh07bhoite@gmail.com>
2025-05-24 23:35:22 +0530

    File Explorer API

--- FileExplorerAPI.js
+++ FileExplorerAPI.js
@@ -7,6 +7,7 @@ const app = express();
 app.get("/files", (req, res) => {
   fs.readdir(".", (err, files) => res.json(files));
 });
+
 app.get("/files/:filename", (req, res) => {
   const filePath = path.join(__dirname, req.params.filename);
   fs.readFile(filePath, "utf8", (err, data) => {
