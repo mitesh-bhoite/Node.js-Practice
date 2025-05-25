53770684934a9b6a4633411b58dc9d25b5deaf1c
mitesh-bhoite <mitesh07bhoite@gmail.com>
2025-05-25 22:32:40 +0530

    REST API with MongoDB

--- RESTAPIwithMongoDB.js
+++ RESTAPIwithMongoDB.js
@@ -4,3 +4,4 @@ const mongoose = require("mongoose");
 const app = express();
 app.use(express.json());
 mongoose.connect("mongodb://localhost:27017/taskdb");
+const Task = mongoose.model("Task", { title: String, done: Boolean });
