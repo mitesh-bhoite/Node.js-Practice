964b40484a42890417a19757e8bc5842aacc6cfe
mitesh-bhoite <mitesh07bhoite@gmail.com>
2025-05-27 22:06:10 +0530

    REST API with MongoDB (CRUD for Tasks)

--- RESTAPIwithMongoDB.js
+++ RESTAPIwithMongoDB.js
@@ -6,3 +6,4 @@ app.use(express.json());
 mongoose.connect("mongodb://localhost:27017/taskdb");
 const Task = mongoose.model("Task", { title: String, done: Boolean });
 app.get("/tasks", async (req, res) => res.json(await Task.find()));
+app.post("/tasks", async (req, res) => res.json(await Task.create(req.body)));
