268b866c994082961871ad486a790633b110d87f
mitesh-bhoite <mitesh07bhoite@gmail.com>
2025-05-28 23:25:52 +0530

    Log Visitor IPs

--- LogVisitorIPs.js
+++ LogVisitorIPs.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const fs = require("fs");
 const app = express();
+
 app.use((req, res, next) => {
   const ip = req.ip;
   fs.appendFileSync("visitors.log", `${new Date()} - ${ip}\n`);
