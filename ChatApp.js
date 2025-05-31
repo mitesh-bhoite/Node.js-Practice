df241b23add02d60d59d94d4716f711b82cfb78a
mitesh-bhoite <mitesh07bhoite@gmail.com>
2025-05-31 22:12:45 +0530

    Chat App

--- ChatApp.js
+++ ChatApp.js
@@ -5,4 +5,6 @@ app.use(express.json());
 const messages = [];
 
 app.get("/chat", (req, res) => res.json(messages));
-app.post("/chat", (req, res) => {});
+app.post("/chat", (req, res) => {
+  messages.push({ user: req.body.user, message: req.body.message });
+});
