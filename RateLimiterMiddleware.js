a0042082fab2c221b4ec76e2ef410e5db76b28fb
mitesh-bhoite <mitesh07bhoite@gmail.com>
2025-06-05 08:31:50 +0530

    Rate Limiter Middleware

--- RateLimiterMiddleware.js
+++ RateLimiterMiddleware.js
@@ -3,4 +3,6 @@ const express = require("express");
 const app = express();
 const requests = {};
 const limit = 5; // max requests per minute
-app.use((req, res, next) => {});
+app.use((req, res, next) => {
+  const ip = req.ip;
+});
