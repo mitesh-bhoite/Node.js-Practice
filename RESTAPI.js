a27e58ab39a61bf30c5406ffad8645175d1569e7
mitesh-bhoite <mitesh07bhoite@gmail.com>
2025-06-10 23:05:14 +0530

    REST API with CRUD for Products

--- RESTAPI.js
+++ RESTAPI.js
@@ -4,5 +4,7 @@ const app = express();
 app.use(express.json());
 let products = [];
 app.get("/products", (req, res) => res.json(products));
-app.post("/products", (req, res) => {});
+app.post("/products", (req, res) => {
+  const product = { id: Date.now(), ...req.body };
+});
 app.listen(3000, () => console.log("Product API running on 3000"));
